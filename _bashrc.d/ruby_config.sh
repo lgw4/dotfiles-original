fe8d5b8782cd7b0b78afaf8f2c0e826094c092fd
Chip Warden <lgw4@introspectiverobot.com>
2012-09-19 11:54:35 -0500

    Refactoring ruby_config.sh logic.

--- _bashrc.d/ruby_config.sh
+++ _bashrc.d/ruby_config.sh
@@ -1,7 +1,10 @@
 # ${HOME}/.bashrc.d/ruby_config.sh file.
 
 # rbenv, rbenv-gemset, and ruby-build
-if [ -e /usr/local/bin/rbenv ] || [ -e ${HOME}/.rbenv/bin/rbenv ]; then
-        eval "$(rbenv init -)"
+if [ -e /usr/local/bin/rbenv ]; then
+    eval "$(rbenv init -)"
+elif [ -e ${HOME}/.rbenv/bin/rbenv ]; then
+    export PATH="${HOME}/.rbenv/bin:${PATH}"
+    eval "$(rbenv init -)"
 fi
 
