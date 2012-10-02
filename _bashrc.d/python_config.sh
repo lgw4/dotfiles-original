4b4d913f553d2b437a67c72172bd2c98401d5d77
Chip Warden <lgw4@introspectiverobot.com>
2012-10-02 08:14:12 -0500

    Added better test for virtualenvwrapper.sh.

--- _bashrc.d/python_config.sh
+++ _bashrc.d/python_config.sh
@@ -12,7 +12,7 @@ fi
 
 # Set up virtualenvwrapper
 VENVWRAPPER=`which virtualenvwrapper.sh`
-if [ -e ${VENVWRAPPER} ]; then
+if [ ${VENVWRAPPER} ]; then
     export VIRTUALENVWRAPPER_PYTHON=`which python`
     export WORKON_HOME="${HOME}/.virtualenvs"
     export PIP_VIRTUALENV_BASE=${WORKON_HOME}
