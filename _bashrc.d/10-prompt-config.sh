b392c65e539c318f06a6f1302b283c370a2dc6bc
Chip Warden <lgw4@introspectiverobot.com>
2012-12-19 10:48:34 -0600

    Better terminal detection responses.

--- _bashrc.d/10-prompt-config.sh
+++ _bashrc.d/10-prompt-config.sh
@@ -2,14 +2,14 @@
 
 # Terminal title bar
 if [ ${TERM} == "linux" ]; then
-    TB="\n"
+    TB=""
 elif [ ${OS_KERNEL} == "Darwin" ]; then
-    TB="\[\033[G\]\[\033]0;\u at \h\007\]\n"
+    TB="\[\033[G\]\[\033]0;\u at \h\007\]"
 elif [ ${OS_KERNEL} == "Linux" ]; then
-    TB="\[\033[G\]\[\033]0;\u@\h: \w\007\]\n"
+    TB="\[\033[G\]\[\033]0;\u@\h: \w\007\]"
 fi
 
 # Custom prompt
-export PS1="${TB}${R}\u ${D}at ${P}\h ${D}in ${B}\
+export PS1="${TB}\n${R}\u ${D}at ${P}\h ${D}in ${B}\
 \W${D} \$(parse_git_branch)\$(parse_svn_status)${D}\n\$ "
 
