ff0fff4dc2576c5ab4d61e846f6aa1d38439e18c
Chip Warden <lgw4@introspectiverobot.com>
2013-01-18 11:04:31 -0600

    Even better checking for rbenv.

--- _bashrc.d/50-ruby-config.sh
+++ _bashrc.d/50-ruby-config.sh
@@ -1,7 +1,8 @@
 # ${HOME}/.bashrc.d/ruby-config.sh file.
 
 # If rbenv, rbenv-gemset, and/or ruby-build are installed, inform PATH
-if [ -e `which rbenv 2> /dev/null` ]; then
+RBENV="`which rbenv 2> /dev/null`"
+if [ ${RBENV} ]; then
     eval "$(rbenv init -)"
 elif [ -e ${HOME}/.rbenv/bin/rbenv ]; then
     PATH="${HOME}/.rbenv/bin:${PATH}"
