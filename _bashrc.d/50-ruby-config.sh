cb2d9795de3cb5de27c9861d8ac832ddbbe8691b
Chip Warden <lgw4@introspectiverobot.com>
2013-01-17 20:18:09 -0600

    Modified rbenv detection.

--- _bashrc.d/50-ruby-config.sh
+++ _bashrc.d/50-ruby-config.sh
@@ -1,7 +1,7 @@
-# ${HOME}/.bashrc.d/ruby_config.sh file.
+# ${HOME}/.bashrc.d/ruby-config.sh file.
 
 # If rbenv, rbenv-gemset, and/or ruby-build are installed, inform PATH
-if [ -e /usr/local/bin/rbenv ]; then
+if [ -e `which rbenv 2> /dev/null` ]; then
     eval "$(rbenv init -)"
 elif [ -e ${HOME}/.rbenv/bin/rbenv ]; then
     PATH="${HOME}/.rbenv/bin:${PATH}"
