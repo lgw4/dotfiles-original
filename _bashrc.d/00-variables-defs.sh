0480a393dfc1f1e962929a3866624020b2e85ec9
Chip Warden <lgw4@introspectiverobot.com>
2014-04-04 21:41:57 -0500

    Use Homebrew curl-ca-bundle certificates.

--- _bashrc.d/00-variables-defs.sh
+++ _bashrc.d/00-variables-defs.sh
@@ -3,6 +3,9 @@
 # Set architecture flags
 export ARCHFLAGS="-arch x86_64"
 
+# Use Homebrew curl-ca-bundle certificates
+export SSL_CERT_FILE=/usr/local/opt/curl-ca-bundle/share/ca-bundle.crt
+
 # Make subl editor, if available
 if `which subl > /dev/null`; then
     export EDITOR="subl -n -w"
