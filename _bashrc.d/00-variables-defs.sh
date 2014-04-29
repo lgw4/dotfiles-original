b1a3a4826770d80290c98e069b9d91419afc50de
Chip Warden <lgw4@introspectiverobot.com>
2014-04-29 13:49:00 -0500

    Add GitHub API token support for Homebrew.

--- _bashrc.d/00-variables-defs.sh
+++ _bashrc.d/00-variables-defs.sh
@@ -3,12 +3,14 @@
 # Set architecture flags
 export ARCHFLAGS="-arch x86_64"
 
-# Use Homebrew curl-ca-bundle certificates
-export SSL_CERT_FILE=/usr/local/opt/curl-ca-bundle/share/ca-bundle.crt
-
 # Make subl editor, if available
 if `which subl > /dev/null`; then
     export EDITOR="subl -n -w"
 else
     export EDITOR="vim"
+fi
+
+# Enable Homebrew GitHub API token, if available
+if [ -e ${HOME}/.homebrew/00-homebrew-github-api-token.sh ]; then
+    source ${HOME}/.homebrew/00-homebrew-github-api-token.sh
 fi
