89ae92ca4c038e6abd9d5a06bb12cf88248a9467
Chip Warden <chip.warden@gmail.com>
2015-05-18 10:17:17 -0500

    Add /sbin and /usr/sbin to PATH on Debian systems.

--- _bashrc.d/00-path-config.sh
+++ _bashrc.d/00-path-config.sh
@@ -5,6 +5,11 @@ if [ `uname` = "Darwin" ]; then
     export PATH="/opt/local/bin:/opt/local/sbin:${PATH}"
 fi
 
+# Add /sbin and /usr/sbin to PATH on Debian systems
+if [ -e /etc/debian_release ]; then
+    export PATH="${PATH}:/sbin:/usr/sbin"
+fi
+
 # Prepend $HOME/.local/bin to PATH
 if [ -d ${HOME}/.local/bin ]; then
     export PATH="${HOME}/.local/bin:${PATH}"
