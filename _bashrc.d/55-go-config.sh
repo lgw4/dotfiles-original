73ec2b78ed33a213de0345425dc3eb04d435b55e
Chip Warden <chip.warden@gmail.com>
2015-05-20 15:12:22 -0500

    Changed GOROOT to /opt/go.

--- _bashrc.d/55-go-config.sh
+++ _bashrc.d/55-go-config.sh
@@ -1,5 +1,10 @@
 # ${HOME}/.bashrc.d/55-go-config.sh
 
+if [ -d /opt/go ]; then
+    export GOROOT="/opt/go"
+    export PATH="${PATH}:${GOROOT}/bin"
+fi
+
 if [ -d "${HOME}/Developer/go" ]; then
     export GOPATH="${HOME}/Developer/go"
     if [ -d "${GOPATH}/bin" ]; then
