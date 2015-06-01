b7286ee283a31d9841a3bfe999016a0b4e68930b
Chip Warden <chip.warden@gmail.com>
2015-06-01 10:32:01 -0500

    Changed EDITOR variable.

--- _bashrc.d/01-variables-defs.sh
+++ _bashrc.d/01-variables-defs.sh
@@ -5,15 +5,5 @@ if [ "${OS_KERNEL}" == "Darwin" ]; then
     export ARCHFLAGS="-arch x86_64"
 fi
 
-# Make MacVim the default visual editor, if available
-if [ "${OS_KERNEL}" == "Darwin" ] && [ `command -v mvim` ] ; then
-    export VISUAL="mvim --nofork"
-fi
-
 export EDITOR="vim"
 
-# Enable Homebrew GitHub API token, if available
-if [ "${OS_KERNEL}" == "Darwin" ] \
-    && [ -e "${HOME}/.homebrew/00-homebrew-github-api-token.sh" ]; then
-        source "${HOME}/.homebrew/00-homebrew-github-api-token.sh"
-fi
