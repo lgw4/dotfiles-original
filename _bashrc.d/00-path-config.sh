768a1102e07cfcfcbc1735983840801ecfc97ba4
Chip Warden <chip.warden@gmail.com>
2015-05-31 21:57:03 -0500

    Corrected GOROOT.

--- _bashrc.d/00-path-config.sh
+++ _bashrc.d/00-path-config.sh
@@ -1,10 +1,5 @@
 # ${HOME}/.bashrc.d/00-path-config.sh
 
-# Add /opt/local/bin and /opt/local/sbin to PATH
-if [ `uname` = "Darwin" ]; then
-    export PATH="/opt/local/bin:/opt/local/sbin:${PATH}"
-fi
-
 # Add /sbin and /usr/sbin to PATH on Debian systems
 if [ -e /etc/debian_version ]; then
     export PATH="${PATH}:/sbin:/usr/sbin"
--- _bashrc.d/55-go-config.sh
+++ _bashrc.d/55-go-config.sh
@@ -1,7 +1,12 @@
 # ${HOME}/.bashrc.d/55-go-config.sh
 
-if [ -d /usr/local/go ]; then
+if [ -d /opt/local/go ]; then
+    export GOROOT="/opt/local/go"
+elif [ -d /usr/local/go ]; then
     export GOROOT="/usr/local/go"
+fi
+
+if [ -n "${GOROOT}" ]; then
     export PATH="${PATH}:${GOROOT}/bin"
 fi
 
