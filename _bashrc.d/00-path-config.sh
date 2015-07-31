a2cb38820aabf99966fb078b6d97940766ab1ae2
Chip Warden <chip.warden@gmail.com>
2015-07-30 21:58:47 -0500

    Once more, back to Homebrew.

--- _bashrc.d/00-path-config.sh
+++ _bashrc.d/00-path-config.sh
@@ -1,10 +1,7 @@
 # ${HOME}/.bashrc.d/00-path-config.sh
 
-# MacPorts on Darwin systems
-if [ -d "/opt/local/bin" ]; then
-    export PATH="/opt/local/bin:/opt/local/sbin:${PATH}"
 # Add /sbin and /usr/sbin to PATH on Debian systems
-elif [ -e /etc/debian_version ]; then
+if [ -e /etc/debian_version ]; then
     export PATH="${PATH}:/sbin:/usr/sbin"
 fi
 
