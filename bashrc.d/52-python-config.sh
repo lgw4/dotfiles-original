7f69fbd10407ccebe2b3f3104fb5919af371868a
Chip Warden <chip.warden@gmail.com>
2016-01-09 17:22:38 -0600

    Clean up commented-out lines.

--- bashrc.d/52-python-config.sh
+++ bashrc.d/52-python-config.sh
@@ -15,21 +15,7 @@ if [ -d "${HOME}/Library/Python/3.5/bin" ]; then
     export PATH="${HOME}/Library/Python/3.5/bin:${PATH}"
 fi
 
-
 # Set up pew
-
 if command -v pew > /dev/null 2>&1; then
     source $(pew shell_config)
 fi
-
-# Set up virtualenvwrapper
-#if command -v virtualenvwrapper.sh > /dev/null 2>&1; then
-#    VENVWRAPPER="$(command -v virtualenvwrapper.sh)"
-#fi
-
-#if [ "${VENVWRAPPER}" ]; then
-#    export VIRTUALENVWRAPPER_PYTHON="$(command -v python3)"
-#    export WORKON_HOME="${HOME}/.virtualenvs"
-#    export PIP_VIRTUALENV_BASE="${WORKON_HOME}"
-#    source "${VENVWRAPPER}"
-#fi
