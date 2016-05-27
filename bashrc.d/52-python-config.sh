109d04d9c1bf8bfa9583e406fcf3163bbee89134
Chip Warden <chip.warden@gmail.com>
2016-05-26 21:16:47 -0500

    Switch back to pew for managing virtual environments.

--- bashrc.d/52-python-config.sh
+++ bashrc.d/52-python-config.sh
@@ -15,11 +15,8 @@ if [ -d "${HOME}/Library/Python/3.5/bin" ]; then
     export PATH="${HOME}/Library/Python/3.5/bin:${PATH}"
 fi
 
-# Set up virtualenvwrapper
-if command -v virtualenvwrapper.sh > /dev/null 2>&1; then
-    export VIRTUALENVWRAPPER_PYTHON=$(command -v python3)
-    export WORKON_HOME="${HOME}/.local/share/virtualenvs"
-    export PIP_VIRTUALENV_BASE="${WORKON_HOME}"
-    export PROJECT_HOME="${HOME}/Developer/python"
-    source $(command -v virtualenvwrapper.sh)
+# Set up pew
+if command -v pew > /dev/null 2>&1; then
+    source $(pew shell_config)
+    complete -o default -F _pew pew
 fi
