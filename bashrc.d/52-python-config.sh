64596395f3fb49b60466be1533f8e005359bdc39
Chip Warden <chip.warden@gmail.com>
2016-05-30 17:12:57 -0500

    Select correct pythonz executable.

--- bashrc.d/52-python-config.sh
+++ bashrc.d/52-python-config.sh
@@ -1,10 +1,5 @@
 # ${HOME}/.bashrc.d/python_config.sh
 
-# pythonz
-if [ -s "${HOME}/.pythonz/etc/bashrc" ]; then
-    source "${HOME}/.pythonz/etc/bashrc"
-fi
-
 # Python 2.7 packages
 if [ -d "${HOME}/Library/Python/2.7/bin" ]; then
     export PATH="${HOME}/Library/Python/2.7/bin:${PATH}"
@@ -13,6 +8,11 @@ fi
 # Python 3.5 packages
 if [ -d "${HOME}/Library/Python/3.5/bin" ]; then
     export PATH="${HOME}/Library/Python/3.5/bin:${PATH}"
+fi
+
+# pythonz
+if [ -s "${HOME}/.pythonz/etc/bashrc" ]; then
+    source "${HOME}/.pythonz/etc/bashrc"
 fi
 
 # Set up pew
