fa206d12447d004586527287602747e810530ed7
Chip Warden <chip.warden@gmail.com>
2016-09-03 20:30:52 -0500

    Remove grid-based logic.

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,104 +1,50 @@
-local application = require "hs.application"
-local fnutils = require "hs.fnutils"
-local grid = require "hs.grid"
-local hotkey = require "hs.hotkey"
-local mjomatic = require "hs.mjomatic"
-local window = require "hs.window"
-
-grid.setGrid('16x10', 'Color LCD')
-
 local mash = {"cmd", "alt", "ctrl"}
 local mashshift = {"cmd", "alt", "ctrl", "shift"}
 
---
--- toggle push window to edge and restore to screen
---
+-- Center frontmost window
+hs.hotkey.bind(mash, "C", function()
+  local win = hs.window.frontmostWindow()
 
--- somewhere to store the original position of moved windows
-local origWindowPos = {}
-
--- cleanup the original position when window restored or closed
-local function cleanupWindowPos(_,_,_,id)
-  origWindowPos[id] = nil
-end
-
--- function to move a window to edge or back
-local function movewin(direction)
-  local win = hs.window.focusedWindow()
-  local res = hs.screen.mainScreen():frame()
-  local id = win:id()
-
-  if not origWindowPos[id] then
-    -- move the window to edge if no original position is stored in
-    -- origWindowPos for this window id
-    local f = win:frame()
-    origWindowPos[id] = win:frame()
-
-    -- add a watcher so we can clean the origWindowPos if window is closed
-    local watcher = win:newWatcher(cleanupWindowPos, id)
-    watcher:start({hs.uielement.watcher.elementDestroyed})
-
-    if direction == "left" then f.x = (res.w - (res.w * 2)) + 10 end
-    if direction == "right" then f.x = (res.w + res.w) - 10 end
-    if direction == "down" then f.y = (res.h + res.h) - 10 end
-    win:setFrame(f)
-  else
-    -- restore the window if there is a value for origWindowPos
-    win:setFrame(origWindowPos[id])
-    -- and clear the origWindowPos value
-    cleanupWindowPos(_,_,_,id)
-  end
-end
-
-hs.hotkey.bind(mash, "A", function() movewin("left") end)
-hs.hotkey.bind(mash, "D", function() movewin("right") end)
-hs.hotkey.bind(mash, "S", function() movewin("down") end)
---
--- /toggle push window to edge and restore to screen
---
-
---
--- Window management
---
---Alter gridsize
--- hotkey.bind(mashshift, '=', function() grid.adjustHeight( 1) end)
--- hotkey.bind(mashshift, '-', function() grid.adjustHeight(-1) end)
--- hotkey.bind(mash, '=', function() grid.adjustWidth( 1) end)
--- hotkey.bind(mash, '-', function() grid.adjustWidth(-1) end)
-
---Snap windows
-hotkey.bind(mash, ';', function() grid.snap(window.focusedWindow()) end)
-hotkey.bind(mash, "'", function() fnutils.map(window.visibleWindows(), grid.snap) end)
-
--- hotkey.bind(mashshift, 'H', function() window.focusedWindow():focusWindowWest() end)
--- hotkey.bind(mashshift, 'L', function() window.focusedWindow():focusWindowEast() end)
--- hotkey.bind(mashshift, 'K', function() window.focusedWindow():focusWindowNorth() end)
--- hotkey.bind(mashshift, 'J', function() window.focusedWindow():focusWindowSouth() end)
-
-hotkey.bind(mash, 'C', function()
-  window.frontmostWindow():centerOnScreen(ensureInScreenBounds)
+  win:centerOnScreen(ensureInScreenBounds)
 end)
 
---Move windows
-hotkey.bind(mash, 'DOWN', grid.pushWindowDown)
-hotkey.bind(mash, 'UP', grid.pushWindowUp)
-hotkey.bind(mash, 'LEFT', grid.pushWindowLeft)
-hotkey.bind(mash, 'RIGHT', grid.pushWindowRight)
+-- Move frontmost window up
+hs.hotkey.bind(mash, "UP", function()
+  local win = hs.window.frontmostWindow()
+  local f = win:frame()
 
---resize windows
-hotkey.bind(mashshift, 'UP', grid.resizeWindowShorter)
-hotkey.bind(mashshift, 'DOWN', grid.resizeWindowTaller)
-hotkey.bind(mashshift, 'RIGHT', grid.resizeWindowWider)
-hotkey.bind(mashshift, 'LEFT', grid.resizeWindowThinner)
+  f.y = f.y - 10
+  win:setFrameInScreenBounds(f)
+end)
 
-hotkey.bind(mash, 'N', grid.pushWindowNextScreen)
-hotkey.bind(mash, 'P', grid.pushWindowPrevScreen)
+-- Move frontmost window down
+hs.hotkey.bind(mash, "DOWN", function()
+  local win = hs.window.frontmostWindow()
+  local f = win:frame()
 
--- hotkey.bind(mashshift, 'M', grid.maximizeWindow)
---
--- /Window management
---
+  f.y = f.y + 10
+  win:setFrameInScreenBounds(f)
+end)
 
+-- Move frontmost window left
+hs.hotkey.bind(mash, "LEFT", function()
+  local win = hs.window.frontmostWindow()
+  local f = win:frame()
+
+  f.x = f.x - 10
+  win:setFrameInScreenBounds(f)
+end)
+
+-- Move frontmost window right
+hs.hotkey.bind(mash, "RIGHT", function()
+  local win = hs.window.frontmostWindow()
+  local f = win:frame()
+
+  f.x = f.x + 10
+  win:setFrameInScreenBounds(f)
+end)
+
+-- Automatically reload configuration on save
 function reloadConfig(files)
     doReload = false
     for _,file in pairs(files) do
@@ -112,7 +58,3 @@ function reloadConfig(files)
 end
 hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
 hs.alert.show("Hammerspoon configuration loaded.")
-
-hs.hotkey.bind({"cmd", "alt"}, "V", function()
-  hs.eventtap.keyStrokes(hs.pasteboard.getContents())
-end)
