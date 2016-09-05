90fdf75d5dc5092412931b48130f4dac59a4614c
Chip Warden <chip.warden@gmail.com>
2016-09-05 12:04:31 -0500

    Remove modal configuration.

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,10 +1,11 @@
 -- Modifier keys
-local action = {"control", "command"}
-local shift_action = {"shift", "control", "command"}
-local move = {"control", "option"}
-local shift_move = {"shift", "control", "option"}
-local resize = {"control", "option", "command"}
-local shift_resize = {"shift", "control", "option", "command"}
+local mv_mod = {"control", "option"}
+local mv_mod_shift = {"control", "option", "shift"}
+local rz_mod = {"control", "command"}
+local rz_mod_shift = {"control", "command", "shift"}
+local act_mod = {"control", "option", "command"}
+local act_mod_shift = {"control", "option", "command", "shift"}
+local no_mod = {}
 
 -- Disable animation
 hs.window.animationDuration = 0
@@ -36,6 +37,13 @@ function resize_window(xpixels, ypixels)
   win:setFrameInScreenBounds(f)
 end
 
+-- Center frontmost window
+function center_window()
+  local win = hs.window.frontmostWindow()
+
+  win:centerOnScreen(ensureInScreenBounds)
+end
+
 -- Move to monitor x. Checks to make sure monitor exists, if not moves to last monitor that exists
 function moveToMonitor(x)
   local win = hs.window.frontmostWindow()
@@ -50,40 +58,36 @@ end
 
 -- Movement hotkeys
 -- Move directonally by 1 pixel
-hs.hotkey.bind(move, "UP", function() move_window(0, -1) end)
-hs.hotkey.bind(move, "DOWN", function() move_window(0, 1) end)
-hs.hotkey.bind(move, "LEFT", function() move_window(-1, 0) end)
-hs.hotkey.bind(move, "RIGHT", function() move_window(1, 0) end)
+hs.hotkey.bind(mv_mod, "UP", function() move_window(0, -1) end)
+hs.hotkey.bind(mv_mod, "DOWN", function() move_window(0, 1) end)
+hs.hotkey.bind(mv_mod, "LEFT", function() move_window(-1, 0) end)
+hs.hotkey.bind(mv_mod, "RIGHT", function() move_window(1, 0) end)
 
 -- Move directionally by 10 pixels
-hs.hotkey.bind(shift_move, "UP", function() move_window(0, -10) end)
-hs.hotkey.bind(shift_move, "DOWN", function() move_window(0, 10) end)
-hs.hotkey.bind(shift_move, "LEFT", function() move_window(-10, 0) end)
-hs.hotkey.bind(shift_move, "RIGHT", function() move_window(10, 0) end)
-
--- Center frontmost window
-hs.hotkey.bind(move, "C", function()
-  local win = hs.window.frontmostWindow()
-
-  win:centerOnScreen(ensureInScreenBounds)
-end)
+hs.hotkey.bind(mv_mod_shift, "UP", function() move_window(0, -10) end)
+hs.hotkey.bind(mv_mod_shift, "DOWN", function() move_window(0, 10) end)
+hs.hotkey.bind(mv_mod_shift, "LEFT", function() move_window(-10, 0) end)
+hs.hotkey.bind(mv_mod_shift, "RIGHT", function() move_window(10, 0) end)
 
 -- Resize hotkeys
 -- Resize directionally by 1 pixel
-hs.hotkey.bind(resize, "UP", function() resize_window(0, -1) end)
-hs.hotkey.bind(resize, "DOWN", function() resize_window(0, 1) end)
-hs.hotkey.bind(resize, "LEFT", function() resize_window(-1, 0) end)
-hs.hotkey.bind(resize, "RIGHT", function() resize_window(1, 0) end)
+hs.hotkey.bind(rz_mod, "UP", function() resize_window(0, -1) end)
+hs.hotkey.bind(rz_mod, "DOWN", function() resize_window(0, 1) end)
+hs.hotkey.bind(rz_mod, "LEFT", function() resize_window(-1, 0) end)
+hs.hotkey.bind(rz_mod, "RIGHT", function() resize_window(1, 0) end)
 
 -- Resize directionally by 10 pixels
-hs.hotkey.bind(shift_resize, "UP", function() resize_window(0, -10) end)
-hs.hotkey.bind(shift_resize, "DOWN", function() resize_window(0, 10) end)
-hs.hotkey.bind(shift_resize, "LEFT", function() resize_window(-10, 0) end)
-hs.hotkey.bind(shift_resize, "RIGHT", function() resize_window(10, 0) end)
+hs.hotkey.bind(rz_mod_shift, "UP", function() resize_window(0, -10) end)
+hs.hotkey.bind(rz_mod_shift, "DOWN", function() resize_window(0, 10) end)
+hs.hotkey.bind(rz_mod_shift, "LEFT", function() resize_window(-10, 0) end)
+hs.hotkey.bind(rz_mod_shift, "RIGHT", function() resize_window(10, 0) end)
+
+-- Center frontmost window
+hs.hotkey.bind(act_mod, "=", function() center_window() end)
 
 -- Manually reload configuration
 -- Found in the "Getting Started with Hammerspoon" guide (http://www.hammerspoon.org/go/)
-hs.hotkey.bind(shift_action, "R", function()
+hs.hotkey.bind(act_mod_shift, "R", function()
   hs.reload()
 end)
 
