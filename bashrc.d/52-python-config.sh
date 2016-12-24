00f84783297f403d25d3eac1c826392f5bdea95b
Chip Warden <chip.warden@gmail.com>
2016-12-24 08:13:32 -0600

    Add support for legacy and current Python user packages.

--- bashrc.d/52-python-config.sh
+++ bashrc.d/52-python-config.sh
@@ -1,13 +1,13 @@
 # ${HOME}/.bashrc.d/python_config.sh
 
-# macOS Python 2.7 packages
-if [ -d "${HOME}/Library/Python/2.7/bin" ]; then
-    export PATH="${HOME}/Library/Python/2.7/bin:${PATH}"
+# macOS legacy Python packages (be sure to create symlink)
+if [ -d "${HOME}/Library/Python/Legacy/bin" ]; then
+    export PATH="${HOME}/Library/Python/Legacy/bin:${PATH}"
 fi
 
-# macOS Python 3 packages (be sure to create symlink)
-if [ -d "${HOME}/Library/Python/3/bin" ]; then
-    export PATH="${HOME}/Library/Python/3/bin:${PATH}"
+# macOS current Python packages (be sure to create symlink)
+if [ -d "${HOME}/Library/Python/Current/bin" ]; then
+    export PATH="${HOME}/Library/Python/Current/bin:${PATH}"
 fi
 
 # pythonz
