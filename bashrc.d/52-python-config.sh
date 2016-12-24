2aba947a1d6cca8f6c4ed906a572c05147883e15
Chip Warden <chip.warden@gmail.com>
2016-12-24 07:59:03 -0600

    Add support for current version of Python 3.

--- bashrc.d/52-python-config.sh
+++ bashrc.d/52-python-config.sh
@@ -1,13 +1,13 @@
 # ${HOME}/.bashrc.d/python_config.sh
 
-# Python 2.7 packages
+# macOS Python 2.7 packages
 if [ -d "${HOME}/Library/Python/2.7/bin" ]; then
     export PATH="${HOME}/Library/Python/2.7/bin:${PATH}"
 fi
 
-# Python 3.5 packages
-if [ -d "${HOME}/Library/Python/3.5/bin" ]; then
-    export PATH="${HOME}/Library/Python/3.5/bin:${PATH}"
+# macOS Python 3 packages (be sure to create symlink)
+if [ -d "${HOME}/Library/Python/3/bin" ]; then
+    export PATH="${HOME}/Library/Python/3/bin:${PATH}"
 fi
 
 # pythonz
