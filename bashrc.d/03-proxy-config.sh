acd11ce5cff52c87e965a533a8dab8027b47e405
Chip Warden <chip.warden@gmail.com>
2017-04-21 12:32:17 -0500

    Add better proxy handling.

--- bashrc.d/03-proxy-config.sh
+++ bashrc.d/03-proxy-config.sh
@@ -3,6 +3,10 @@
 if command -v networksetup > /dev/null 2>&1; then
     if [[ `networksetup -getcurrentlocation` == "DST" ]]; then
         export http_proxy="dstproxy.dstcorp.net:9119"
-        export https_proxy="dstproxy.dstcorp.net:9119"
+        export https_proxy="${http_proxy}"
+        export no_proxy="*.local, 169.254.0.0/16, 172.30.0.0/16, *.ad.dstsystems.com, *.dstcorp.net"
+        export HTTP_PROXY="${http_proxy}"
+        export HTTPS_PROXY="${https_proxy}"
+        export NO_PROXY="${no_proxy}"
     fi
 fi
