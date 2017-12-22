2c371b2dbb1f01957c2499f80ff63fe22f77d0c2
Chip Warden <chip.warden@gmail.com>
2017-12-22 13:33:21 -0600

    Switch back to virtualenvwrapper.

--- bashrc.d/50-python-config.sh
+++ bashrc.d/50-python-config.sh
@@ -13,9 +13,13 @@ fi
 # pythonz
 [[ -s ${HOME}/.pythonz/etc/bashrc ]] && source ${HOME}/.pythonz/etc/bashrc
 
-# Enable pew
-if command -v pew > /dev/null 2>&1; then
+# Enable virtualenvwrapper
+if command -v virtualenvwrapper.sh > /dev/null 2>&1; then
     export WORKON_HOME="${HOME}/.local/share/virtualenvs"
     export PROJECT_HOME="${HOME}/Developer/python"
-    source "$(pew shell_config)"
+    export VIRTUALENVWRAPPER_PYTHON="$(which python3)"
+    export VIRTUALENVWRAPPER_VIRTUALENV="$(which virtualenv)"
+    export VIRTUALENVWRAPPER_VIRTUALENV_ARGS="--no-site-packages"
+    export VIRTUALENVWRAPPER_HOOK_DIR="${WORKON_HOME}/.hooks"
+    source "$(which virtualenvwrapper.sh)"
 fi
