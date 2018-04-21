ba9cb31015c9d601c4a12cf2117244a89b4e0c00
Chip Warden <chip.warden@gmail.com>
2018-04-20 22:29:38 -0500

    Configuration change for pip 10.

--- bashrc.d/00-path-config.sh
+++ bashrc.d/00-path-config.sh
@@ -1,14 +1,14 @@
 # -*- mode: sh; -*-
 
 if [ "${LOGIN_SHELL}" == "True" ]; then
-    # Check for /usr/local/bin in PATH
-    if [[ ":${PATH}:" != *":/usr/local/bin:"* ]]; then
-        export PATH="/usr/local/bin:${PATH}"
-    fi
-
     # Check for /usr/local/sbin in PATH
     if [[ ":${PATH}:" != *":/usr/local/sbin:"* ]]; then
         export PATH="/usr/local/sbin:${PATH}"
+    fi
+
+    # Check for /usr/local/bin in PATH
+    if [[ ":${PATH}:" != *":/usr/local/bin:"* ]]; then
+        export PATH="/usr/local/bin:${PATH}"
     fi
 
     # Add /sbin and /usr/sbin to PATH on Debian systems
