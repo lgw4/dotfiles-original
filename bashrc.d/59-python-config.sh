a5864c35d70b790462cb40e5297fc223e360e17f
Chip Warden <chip.warden@gmail.com>
2019-05-03 16:52:24 -0500

    More pyenv shenannigans.

--- bashrc.d/59-python-config.sh
+++ bashrc.d/59-python-config.sh
@@ -23,7 +23,7 @@ fi
 # Enable pyenv
 if [[ ! -v VIRTUAL_ENV ]]; then
     add_to_path "${HOME}/.pyenv/bin"
-    if command -v pyenv > /dev/null 2>&1; then
+    if command -v pyenv > /dev/null 2>&1 && [[ ":${PATH}:" != *":${HOME}/.pyenv/shims:"* ]]; then
         eval "$(pyenv init -)"
         export PYENV_VIRTUALENV_DISABLE_PROMPT=1
     fi
