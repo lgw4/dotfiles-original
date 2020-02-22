f714d6d696c7ee03f7bd2fb7194bee506bf19cce
Chip Warden <chip.warden@gmail.com>
2020-02-21 18:02:31 -0600

    Reorganize Zsh Python config files.

--- zshenv.d/02-homebrew-openssl.zsh
+++ zshenv.d/02-homebrew-openssl.zsh
@@ -1,5 +1,8 @@
 if [[ -d "/usr/local/opt/openssl@1.1/" ]]; then
-    export CPPFLAGS="-I/usr/local/opt/openssl@1.1/include ${CPPFLAGS}"
-    export LDFLAGS="-L/usr/local/opt/openssl@1.1/lib ${LDFLAGS}"
-    export PKG_CONFIG_PATH="/usr/local/opt/openssl@1.1/lib/pkgconfig ${PKG_CONFIG_PATH}"
+    cppflags+=("-I/usr/local/opt/openssl@1.1/include")
+    ldflags+=("-L/usr/local/opt/openssl@1.1/lib")
+    pkg_config_path+=("/usr/local/opt/openssl@1.1/lib/pkgconfig")
+    export CPPFLAGS
+    export LDFLAGS
+    export PKG_CONFIG_PATH
 fi
---
+++ zshrc.d/61-python-pyenv.zsh
@@ -0,0 +1,7 @@
+if [[ -d "${HOME}/.pyenv/bin" ]]; then
+    add_to_path "${HOME}/.pyenv/bin"
+fi
+if command -v pyenv > /dev/null 2>&1 && [[ ":${PATH}:" != *":${HOME}/.pyenv/shims:"* ]]; then
+    eval "$(pyenv init -)"
+    export PYENV_VIRTUALENV_DISABLE_PROMPT=1
+fi
