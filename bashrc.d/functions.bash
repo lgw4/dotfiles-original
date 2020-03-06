ea15c4a21e5851eda0859e75a99d2fb6f423223a
Chip Warden <chip.warden@gmail.com>
2020-03-05 20:15:31 -0600

    Minor Bash configuration reorganization.

--- bashrc.d/functions.bash
+++ bashrc.d/functions.bash
@@ -1,17 +1,26 @@
 generate_prompt() {
-    # preserve history across sessions
+    # Preserve history across sessions
     builtin history -a
     builtin history -c
     builtin history -r
-    # clear PS1 prompt
+    # Terminal color codes
+    blue="\[\e[0;34m\]"
+    cyan="\[\e[0;36m\]"
+    default="\[\e[0m\]"
+    green="\[\e[0;32m\]"
+    orange="\[\e[0;38;5;166m\]"
+    purple="\[\e[0;35m\]"
+    red="\[\e[0;31m\]"
+    yellow="\[\e[0;33m\]"
+    # Clear PS1 prompt
     PS1=""
-    # terminal title bar
+    # Terminal title bar
     if [[ "$TERM" == "linux" ]] || [[ "$TERM" == "eterm-color" ]]; then
         PS1+="\n"
     else
         PS1+="\[\e[G\]\[\e]0;\u@\h: \w\007\]\n"
     fi
-    # basic information (user at host in path)
+    # Basic information (user at host in path)
     PS1+="${red}\u${default} at ${purple}\h${default} in ${blue}\W${default} "
     # Git status (if available)
     if [[ "$OS_KERNEL" == "Darwin" ]] && command -v brew > /dev/null 2>&1; then
@@ -32,8 +41,17 @@ generate_prompt() {
     if [[ -v VIRTUAL_ENV ]]; then
         PS1+="${default}(${orange}$(basename "$VIRTUAL_ENV")${default})"
     fi
-    # prompt $ or # for root
+    # Prompt ($ for regular users or # for root)
     PS1+="\n\$ "
+
+    unset -v blue
+    unset -v cyan
+    unset -v default
+    unset -v green
+    unset -v orange
+    unset -v purple
+    unset -v red
+    unset -v yellow
 }
 
 append_path() {
---
+++ bashrc.d/vars-editor.bash
@@ -0,0 +1,5 @@
+if command -v code > /dev/null 2>&1 && [[ ! -v SSH_CONNECTION ]]; then
+    export EDITOR="code"
+else
+    export EDITOR="vim"
+fi
