bb69a8ffe225c4a867a955657d9ebcf7eec19d94
Chip Warden <lgw4@icloud.com>
2020-04-27 09:53:18 -0500

    Improve libpq support.

--- bashrc.d/path-libpq.bash
+++ bashrc.d/path-libpq.bash
@@ -1,4 +1,6 @@
 # shellcheck shell=bash
-if ! command -v psql > /dev/null 2>&1; then
+if ! command -v psql > /dev/null 2>&1 && [[ -d "/usr/local/opt/libpq" ]]; then
+    export CPPFLAGS="-I/usr/local/opt/libpq/include ${CPPFLAGS}"
+    export LDFLAGS="-L/usr/local/opt/libpq/lib ${LDFLAGS}"
     append_path /usr/local/opt/libpq/bin
 fi
