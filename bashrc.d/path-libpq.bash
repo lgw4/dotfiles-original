7e5a7ab7447c904b77a6a3b5b60bf83aebc035ea
Chip Warden <lgw4@icloud.com>
2020-04-27 14:12:37 -0500

    Use correct function.

--- bashrc.d/path-libpq.bash
+++ bashrc.d/path-libpq.bash
@@ -2,5 +2,5 @@
 if ! command -v psql > /dev/null 2>&1 && [[ -d "/usr/local/opt/libpq" ]]; then
     export CPPFLAGS="-I/usr/local/opt/libpq/include ${CPPFLAGS}"
     export LDFLAGS="-L/usr/local/opt/libpq/lib ${LDFLAGS}"
-    append_path /usr/local/opt/libpq/bin
+    path_append /usr/local/opt/libpq/bin PATH
 fi
