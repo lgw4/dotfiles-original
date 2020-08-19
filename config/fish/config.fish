c572b35e1f9161069d4ce04e8aee1f69af2c7cbc
Chip Warden <lgw4@icloud.com>
2020-08-19 10:30:47 -0500

    Add fish_right_prompt.

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,1 +1,6 @@
 set -U fish_prompt_pwd_dir_length 0
+
+if status is-login
+    echo
+    fortune
+end
---
+++ config/fish/functions/fish_right_prompt.fish
@@ -0,0 +1,4 @@
+function fish_right_prompt
+    set -g __fish_git_prompt_showdirtystate
+    fish_git_prompt
+end
