7d64ad835ba9ed1181a00f38cd2cdcbf2ed4451d
Chip Warden <lgw4@icloud.com>
2020-08-20 09:03:36 -0500

    Add virtualfish support.

---
+++ config/fish/conf.d/virtualfish-loader.fish
@@ -0,0 +1,4 @@
+set -g VIRTUALFISH_VERSION 2.4.0
+set -g VIRTUALFISH_PYTHON_EXEC $HOME/.pyenv/versions/3.8.5/bin/python3
+source $HOME/.local/lib/python3.8/site-packages/virtualfish/virtual.fish
+emit virtualfish_did_setup_plugins
