0939ed07bcace2ed0706a82893f4339b660b1618
Chip Warden <lgw4@icloud.com>
2020-08-20 09:37:58 -0500

    Improve virtualfish support.

--- config/fish/conf.d/virtualfish-loader.fish
+++ config/fish/conf.d/virtualfish-loader.fish
@@ -1,4 +1,14 @@
-set -g VIRTUALFISH_VERSION 2.4.0
-set -g VIRTUALFISH_PYTHON_EXEC $HOME/.pyenv/versions/3.8.5/bin/python3
-source $HOME/.local/lib/python3.8/site-packages/virtualfish/virtual.fish
-emit virtualfish_did_setup_plugins
+if test -e $HOME/.local/lib/python3.8/site-packages/virtualfish/virtual.fish
+    set -g VIRTUALFISH_VERSION 2.4.0
+    set -g VIRTUALFISH_PYTHON_EXEC $HOME/.pyenv/versions/3.8.5/bin/python3
+    if not test -d $HOME/.local/share/virtualenvs
+        mkdir -p $HOME/.local/share/virtualenvs
+    end
+    set -g VIRTUALFISH_HOME $HOME/.local/share/virtualenvs
+    source $HOME/.local/lib/python3.8/site-packages/virtualfish/virtual.fish
+    source $HOME/.local/lib/python3.8/site-packages/virtualfish/auto_activation.fish
+    source $HOME/.local/lib/python3.8/site-packages/virtualfish/environment.fish
+    source $HOME/.local/lib/python3.8/site-packages/virtualfish/projects.fish
+    source $HOME/.local/lib/python3.8/site-packages/virtualfish/update_python.fish
+    emit virtualfish_did_setup_plugins
+end
