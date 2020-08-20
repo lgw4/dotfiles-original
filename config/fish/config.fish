325de112f05153f30f657d5f0613f8164095eedf
Chip Warden <lgw4@icloud.com>
2020-08-20 12:30:29 -0500

    Add pipx completions for Fish shell.

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -8,3 +8,7 @@ end
 if test -e $HOME/.iterm2_shell_integration.fish
     source $HOME/.iterm2_shell_integration.fish
 end
+
+if type -q pipx
+    register-python-argcomplete --shell fish pipx | source
+end
