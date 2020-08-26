83b3469f314b7ded36cbc5c0046141a7f27d0ad5
Chip Warden <lgw4@icloud.com>
2020-08-26 17:04:41 -0500

    Show terminal dimensions in title if on iTerm.

--- config/fish/functions/fish_title.fish
+++ config/fish/functions/fish_title.fish
@@ -4,4 +4,7 @@ function fish_title -d "Set terminal application window title"
     printf '%s' (prompt_hostname)
     printf ': '
     printf '%s' (prompt_pwd)
+    if test "$TERM_PROGRAM" = "iTerm.app"
+        printf ' [%s x %s]' $COLUMNS $LINES
+    end
 end
