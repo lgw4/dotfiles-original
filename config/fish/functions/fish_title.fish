919129caf6339f6b8e905b0bba252b6a7e6ef152
Chip Warden <lgw4@icloud.com>
2020-08-26 19:52:10 -0500

    Update terminal checking in Fish shell.

--- config/fish/functions/fish_title.fish
+++ config/fish/functions/fish_title.fish
@@ -4,7 +4,7 @@ function fish_title -d "Set terminal application window title"
     printf '%s' (prompt_hostname)
     printf ': '
     printf '%s' (prompt_pwd)
-    if test "$TERM_PROGRAM" = "iTerm.app"
-        printf ' [%s x %s]' $COLUMNS $LINES
+    if test "$LC_TERMINAL" = "iTerm2"
+        printf ' — %s×%s' $COLUMNS $LINES
     end
 end
