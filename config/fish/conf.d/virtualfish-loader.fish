c3c78c1ebfd14a4cd35fd23399cd324ff5ba4f6c
Chip Warden <lgw4@icloud.com>
2020-09-21 19:29:09 -0500

    Correct error in virtualfish-loader.fish VIRTUALFISH_PYTHON_EXEC variable.

--- config/fish/conf.d/virtualfish-loader.fish
+++ config/fish/conf.d/virtualfish-loader.fish
@@ -8,7 +8,7 @@ if not test -d $HOME/.local/share/virtualenvs
 end
 set -g VIRTUALFISH_HOME $HOME/.local/share/virtualenvs
 set -g VIRTUALFISH_VERSION 2.5.0
-set -g VIRTUALFISH_PYTHON_EXEC /Users/lgw4/.pyenv/versions/3.8.5/bin/python3
+set -g VIRTUALFISH_PYTHON_EXEC $HOME/.pyenv/versions/3.8.5/bin/python3
 source /Users/lgw4/.local/lib/python3.8/site-packages/virtualfish/virtual.fish
 source /Users/lgw4/.local/lib/python3.8/site-packages/virtualfish/auto_activation.fish
 source /Users/lgw4/.local/lib/python3.8/site-packages/virtualfish/projects.fish
