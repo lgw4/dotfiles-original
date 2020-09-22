d95619f20d476c1ae8a593a56067abd11a3065b1
Chip Warden <lgw4@icloud.com>
2020-09-21 19:33:06 -0500

    Correct more problems with autogenerated virtualfish-loader.fish.

--- config/fish/conf.d/virtualfish-loader.fish
+++ config/fish/conf.d/virtualfish-loader.fish
@@ -9,8 +9,8 @@ end
 set -g VIRTUALFISH_HOME $HOME/.local/share/virtualenvs
 set -g VIRTUALFISH_VERSION 2.5.0
 set -g VIRTUALFISH_PYTHON_EXEC $HOME/.pyenv/versions/3.8.5/bin/python3
-source /Users/lgw4/.local/lib/python3.8/site-packages/virtualfish/virtual.fish
-source /Users/lgw4/.local/lib/python3.8/site-packages/virtualfish/auto_activation.fish
-source /Users/lgw4/.local/lib/python3.8/site-packages/virtualfish/projects.fish
-source /Users/lgw4/.local/lib/python3.8/site-packages/virtualfish/update_python.fish
+source $HOME/.local/lib/python3.8/site-packages/virtualfish/virtual.fish
+source $HOME/.local/lib/python3.8/site-packages/virtualfish/auto_activation.fish
+source $HOME/.local/lib/python3.8/site-packages/virtualfish/projects.fish
+source $HOME/.local/lib/python3.8/site-packages/virtualfish/update_python.fish
 emit virtualfish_did_setup_plugins
