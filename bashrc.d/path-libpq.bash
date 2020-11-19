77d373ddb2589c255ac4cc9ccc4855bc4479cc92
Chip Warden <lgw4@icloud.com>
2020-11-18 22:19:36 -0600

    Add missing brace.

--- bashrc.d/path-libpq.bash
+++ bashrc.d/path-libpq.bash
@@ -1,5 +1,5 @@
 # shellcheck shell=bash
-if ! command -v psql >/dev/null 2>&1 && [[ -d "${HOMEBREW_PREFIX/opt/libpq" ]]; then
+if ! command -v psql >/dev/null 2>&1 && [[ -d "${HOMEBREW_PREFIX}/opt/libpq" ]]; then
     export CPPFLAGS="-I${HOMEBREW_PREFIX}/opt/libpq/include ${CPPFLAGS}"
     export LDFLAGS="-L${HOMEBREW_PREFIX}/opt/libpq/lib ${LDFLAGS}"
     path_append ${HOMEBREW_PREFIX}/opt/libpq/bin PATH
