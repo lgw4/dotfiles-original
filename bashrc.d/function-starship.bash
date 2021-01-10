ec423a55f57de5bdda61a38ec5013bb8a06ec092
Chip Warden <lgw4@icloud.com>
2021-01-09 19:00:06 -0600

    Collect all functions to support Apple Silicon into one place.

--- bashrc.d/function-starship.bash
+++
@@ -1,5 +0,0 @@
-if [[ $OSTYPE == darwin* ]] && [[ -f /opt/homebrew/bin/starship ]]; then
-    starship() {
-        /opt/homebrew/bin/starship "$@"
-    }
-fi
---
+++ bashrc.d/functions-apple-silicon.bash
@@ -0,0 +1,41 @@
+# shellcheck shell=bash
+if [[ $OSTYPE == darwin* ]]; then
+    if [[ -f /opt/homebrew/bin/brew ]]; then
+        brew-arm64() {
+            HOMEBREW_NO_ENV_FILTERING=1 /opt/homebrew/bin/brew "$@"
+        }
+    fi
+
+    if [[ -f /usr/local/bin/brew ]] && [[ $(arch) == arm64 ]]; then
+        brew() {
+            /usr/bin/arch -x86_64 /usr/local/bin/brew "$@"
+        }
+        bash-x86_64() {
+            /usr/bin/arch -x86_64 /usr/local/bin/bash
+        }
+    fi
+
+    if [[ -f /opt/homebrew/bin/starship ]]; then
+        starship() {
+            /opt/homebrew/bin/starship "$@"
+        }
+    fi
+
+    if [[ -f /opt/homebrew/bin/git ]]; then
+        git() {
+            /opt/homebrew/bin/git "$@"
+        }
+    fi
+
+    if [[ -f /opt/homebrew/bin/tree ]]; then
+        tree() {
+            /opt/homebrew/bin/tree "$@"
+        }
+    fi
+
+    if [[ -f /opt/homebrew/bin/bat ]]; then
+        bat() {
+            /opt/homebrew/bin/bat "$@"
+        }
+    fi
+fi
