37723eeaa47c0ef49f4195f985856fc262c3e0bd
Chip Warden <lgw4@icloud.com>
2021-02-26 14:35:55 -0600

    Update pip completion.

--- bashrc.d/path-python.bash
+++ bashrc.d/path-python.bash
@@ -15,8 +15,8 @@ fi
 # Enable pip completion
 _pip_completion() {
     COMPREPLY=($(COMP_WORDS="${COMP_WORDS[*]}" \
-        COMP_CWORD="$COMP_CWORD" \
-        PIP_AUTO_COMPLETE=1 "$1"))
+        COMP_CWORD="${COMP_CWORD}" \
+        PIP_AUTO_COMPLETE=1 "${1}" 2>/dev/null))
 }
 complete -o default -F _pip_completion pip
 complete -o default -F _pip_completion pip3
