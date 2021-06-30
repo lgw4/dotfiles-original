f4da5ac7a75a7252a13d2b49204a74f053567567
Chip Warden <lgw4@icloud.com>
2021-06-29 21:16:17 -0500

    Make various unrelated updates.

--- config/fish/conf.d/path-base.fish
+++ config/fish/conf.d/path-base.fish
@@ -1,15 +1,15 @@
 path_prepend /usr/local/sbin
 path_prepend /usr/local/bin
 
-if test (uname) = 'Darwin'
-    if test (/usr/bin/arch) = 'arm64'
+if test (uname) = Darwin
+    if test (/usr/bin/arch) = arm64
         and not test -f /usr/local/bin/brew
         set -x HOMEBREW_ROOT /opt/homebrew
     else
         set -x HOMEBREW_ROOT /usr/local
     end
 
-    if test "$HOMEBREW_ROOT" = '/usr/local'
+    if test "$HOMEBREW_ROOT" = /usr/local
         path_append /opt/homebrew/bin
         path_append /opt/homebrew/sbin
     else
@@ -22,7 +22,7 @@ if test (uname) = 'Darwin'
     end
 end
 
-if test (uname) = 'Linux'
+if test (uname) = Linux
     if test -e /etc/debian_version
         path_prepend /sbin
         path_prepend /usr/sbin
